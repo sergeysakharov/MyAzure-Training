5c44025d48e1ff99c91277b01c6633756b7486d4
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2021-08-13 17:10:54 +0300

    Add files via upload

---
+++ TestImport/Main.tf
@@ -0,0 +1,27 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.72.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  tenant_id       = "17e32440-c4c6-487c-a361-41272e6db574"
+  subscription_id = "8849218e-7382-42ab-9434-352bfc1e1c68"
+  client_id       = "750d8ec5-2538-4168-8dba-d6665886ebf4"
+  client_secret   = "TEIpitVAVLI_CcjAA2yyVZWy-MmUw8lb_1"
+  features {}
+}
+
+# create resource group
+resource "azurerm_resource_group" "az-fd-prod1"{
+    name = "az-fd-prod1"
+    location = "Westus"
+}
+resource "azurerm_resource_group" "az-fd-prod2"{
+    name = "az-fd-prod2"
+    location = "Westus"
+}
